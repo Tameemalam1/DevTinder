9b6a59010f5e161e66a41017f7da1fdc25ae3327
tameemh360l <tameem@sdilabsinc.com>
2025-07-29 19:06:33 +0530

    exploring route handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req,res) => {
-    res.send("Hello from dashboard");
+app.get("/user",(req,res) => {
+    res.send("User Data");
+});
+
+app.post("/user",(req,res) => {
+    res.send("User Data saved succesfully");
+});
+
+app.put("/user",(req,res) => {
+    res.send("User Data putted succesfully");
+});
+
+app.patch("/user",(req,res) => {
+    res.send("User Data patched succesfully");
+});
+
+app.delete("/user",(req,res) => {
+    res.send("User Data deleted succesfully");
 });
 
 app.use("/test", (req,res) => {
     res.send("Hello Test");
-});
-
-app.use("/hello", (req,res) => {
-    res.send("Hello Hello");
 });
 
 app.listen(7777,() => {
